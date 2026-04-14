7818f62b3c811b1c9f77f777ffc9c30dfd9b99b5
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-04-14 20:36:01 +0200

    logoimage

---
+++ src/Pharmacy.Infrastructure/Migrations/20260414183516_logoimage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharmacy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class logoimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoImage",
+                table: "Branches",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoImage",
+                table: "Branches");
+        }
+    }
+}
